6a655f54cd2c172279588cd2cb0d29ed9ad60454
Pármenas Haniel <parmenashp@gmail.com>
2023-05-05 17:57:41 -0300

    Fix entrypoint using wrong password

--- mongo/entrypoint.js
+++ mongo/entrypoint.js
@@ -3,6 +3,6 @@
 // - Collection name: filtered_tweets
 // - Create index on "author_id" field in "filtered_tweets"
 
-db.auth("furretweet", "areallygoodpasswordhere");
+db.auth("furretweet", process.env.MONGO_DB_PASSWORD);
 
 db.not_retweeted_tweets.createIndex({ author_id: 1 }, { unique: false });
